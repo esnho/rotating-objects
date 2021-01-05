35c306916c4d7c8fe6a8593a9df29d9019cd96a7
esnho <esnhoesnho@gmail.com>
2021-01-05 19:11:11 +0100

    Update

--- animations/convert-all.sh
+++ animations/convert-all.sh
@@ -9,7 +9,11 @@ for folder in ./* ; do
     fBasename=$(basename "${folder}")
     echo -e "${fBasename} ---"
     if [ ! -f "$fBasename".mp4 ]; then
-      ffmpeg -r 24 -i "${folder}/%04d.png" -c:v libx264 -q:v 0 -g 1 -vf fps=24 -pix_fmt yuv420p ${fBasename}.mp4
+      # ffmpeg -r 24 -i "${folder}/%04d.png" -c:v libx264 -q:v 0 -g 1 -vf fps=24 -pix_fmt yuv420p ${fBasename}.mp4
+      
+      # 4:3 aspect ratio
+      ffmpeg -r 24 -i "${folder}/%04d.png" -c:v libx264 -q:v 0 -g 1 -vf fps=24 -filter:v "crop=960:in_h" -pix_fmt yuv420p ${fBasename}.mp4
+
       # ffmpeg -r 24 -i "./Cube/%04d.png" -c:v libx264 -q:v 0 -g 1 -vf fps=24 -pix_fmt yuv420p test.mp4
     fi
   fi
