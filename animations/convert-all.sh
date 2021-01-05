932a5c50b9aff4d7da4017fbf4ae5caca61dfcd5
esnho <esnhoesnho@gmail.com>
2021-01-05 14:09:52 +0100

    first commit

---
+++ animations/convert-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for folder in ./* ; do
+  if [ ! -f "${folder}" ]; then
+    echo -e "not a file"
+    echo -e "${folder}"
+  # ffmpeg -i input.file -q:v 0 -g 1 output.file
+  # ffmpeg -r 1/5 -i img%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p out.mp4
+    fBasename=$(basename "${folder}")
+    echo -e "${fBasename} ---"
+    if [ ! -f "$fBasename".mp4 ]; then
+      ffmpeg -r 24 -i "${folder}/%04d.png" -c:v libx264 -q:v 0 -g 1 -vf fps=24 -pix_fmt yuv420p ${fBasename}.mp4
+      # ffmpeg -r 24 -i "./Cube/%04d.png" -c:v libx264 -q:v 0 -g 1 -vf fps=24 -pix_fmt yuv420p test.mp4
+    fi
+  fi
+done
